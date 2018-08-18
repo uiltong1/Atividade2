3adf8fa3f60603d761d637edfcc26487d15b8252
Uilton Gomes <uilton-gomes@hotmail.com>
2018-08-18 10:59:37 -0300

    Adicionando mapa

---
+++ js/mapa.js
@@ -0,0 +1,23 @@
+var mymap = L.map('mapid').setView([-12.9704, -38.5124], 10);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+var x =document.getElementById("mapid");
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+
+  function showPosition(position){
+    x ="Latitude: " + position.coords.latitude +
+    "Longitude: " + position.coords.longitude;
+    console.log(x);
+    var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap);
+
+  }
